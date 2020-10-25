1891e114171244d68b9d547f29959772c1f7b0b6
Daniel Rodgers-Pryor <djrodgerspryor@gmail.com>
2020-10-25 22:52:44 +1100

    Working automated transcription via AWS
    But it's only single-threaded for now, so I haven't invested too much in the UX. The next step is to run it in parallel, with multiple progress bars, and handling for queue full errors and similar.

--- speech-grep.py
+++ speech-grep.py
@@ -6,6 +6,17 @@ import mimetypes
 import glob
 import xxhash
 import sys
+import boto3
+from botocore.errorfactory import ClientError as BotoClientError
+import re
+import collections
+import json
+import time
+from urllib.parse import urlparse
+
+TranscriptionConfig = collections.namedtuple("TranscriptionConfig", ["provider", "s3_bucket", "input_prefix", "output_prefix"])
+
+SAVED_CONFIG_LOCATION = os.path.expanduser('~/.speech-grep')
 
 SUPPORTED_FORMATS = set([
     'audio/mpeg',
@@ -17,6 +28,21 @@ SUPPORTED_FORMATS = set([
     'audio/x-flac',
     'audio/x-wav',
 ])
+
+class TranscriptionOptions:
+    # TODO: maybe support vocabularies etc.
+    def __init__(self, language_code=None, max_alternatives=0):
+        self.language_code = language_code
+        self.max_alternatives = max_alternatives
+
+    def digest(self):
+        # Hash all of the options in a consistent order
+        return xxhash.xxh32(
+            "{language_code:}{max_alternatives:}".format(
+                language_code=self.language_code,
+                max_alternatives=self.max_alternatives
+            )
+        ).digest()
 
 """Generous handling of cli paths, globs, directories etc. to product a list
 of audio files to work with"""
@@ -62,6 +88,7 @@ def select_input_files(inputs, ignore_type):
     return input_files
 
 FILE_CONTENT_HASH_CACHE = {}
+"""Returns a hash of file content. Cached against the given file name."""
 def cached_hash_file_content(normalised_path):
     if normalised_path not in FILE_CONTENT_HASH_CACHE:
         with open(normalised_path, 'rb') as f:
@@ -69,39 +96,222 @@ def cached_hash_file_content(normalised_path):
 
     return FILE_CONTENT_HASH_CACHE[normalised_path]
 
+"""Get the path for an existing transcript file if there is one for this file/query combo"""
 def get_existing_transcript(input_file, transcript_query_hash):
-    content_hash = cached_hash_file_content(input_file)
-
-    # Combine the file content hash, with the query hash to get a hash that we can use to cache
-    # transcripts with proper invalidation characteristics
-    expected_transcript_hash_hex = xxhash.xxh32(content_hash + transcript_query_hash).hexdigest()
-
     # Look for any existing transcript files with this hash in the filename (the prefix can be anything — usually the original file name)
     input_directory = os.path.dirname(input_file)
     return next(iter(
         glob.glob(
             os.path.join(
                 input_directory,
-                "*-transcript-{}.json".format(expected_transcript_hash_hex)
+                "*-{}.transcript.json".format(transcript_hash(input_file, transcript_query_hash))
             )
         )
     ), None)
 
-def ensure_transcripts(input_files):
-    transcript_paths = {}
+def transcript_hash(input_file, transcript_query_hash):
+    content_hash = cached_hash_file_content(input_file)
+
+    # Combine the file content hash, with the query hash to get a hash that we can use to cache
+    # transcripts with proper invalidation characteristics
+    return xxhash.xxh32(content_hash + transcript_query_hash).hexdigest()
+
+def transcript_name(input_file, transcript_query_hash):
+    # Get the input filename, without extension
+    filename = os.path.splitext(os.path.basename(input_file))[0]
+
+    # Combine the original filename (so that the transcript will sort next to the original file
+    # and so that it's relationship will be obvious) with the digest which ensures that the transcript
+    # is unique for this kind of query and this file content.
+    return "{}-{}.transcript.json".format(filename, transcript_hash(input_file, transcript_query_hash))
+
+"""Get transcript files for all of the inputs, either from a local cache or by generating them"""
+def ensure_transcripts(input_files, transcription_options, transcription_config):
+    transcript_path_by_input_path = {}
+
+    options_digest = transcription_options.digest()
 
     # This might take a little time to read and hash all of the audio content, so
     # display a progress bar
     with click.progressbar(input_files, label="Checking for existing transcripts", file=sys.stderr) as input_files_to_check:
         for input_file in input_files_to_check:
-            maybe_path = get_existing_transcript(input_file, b'TODO')
+            maybe_path = get_existing_transcript(input_file, options_digest)
 
             if maybe_path is not None:
-                transcript_paths[input_file] = maybe_path
+                transcript_path_by_input_path[input_file] = maybe_path
 
-    to_transcode = [input_file for input_file in input_files if input_file not in transcript_paths]
+    to_transcode = [input_file for input_file in input_files if input_file not in transcript_path_by_input_path]
 
     print(to_transcode)
+
+    for input_file, transcript_file in zip(to_transcode, batch_transcribe(to_transcode, transcription_options, transcription_config)):
+        transcript_path_by_input_path[input_file] = transcript_file
+
+    return transcript_path_by_input_path
+
+def batch_transcribe(input_files, transcription_options, transcription_config):
+    click.echo("Preparing to transcribe {} files".format(len(input_files)), err=True)
+
+    # TODO: Parallel with parallel progress bars
+    results = []
+    for input_file in sorted(list(input_files)):
+        print(input_file)
+        results.append(
+            transcribe(input_file, transcription_options, transcription_config, upload_progress_callback=lambda x: None, transcribe_progress_callback=lambda x: None)
+        )
+
+
+class ExistingS3ObjectInvalid(Exception):
+    pass
+
+def transcribe(input_file, transcription_options, transcription_config, upload_progress_callback=lambda x: None, transcribe_progress_callback=lambda x: None):
+    aws_s3_client = boto3.client('s3')
+    aws_transcribe_client = boto3.client('transcribe')
+
+    file_size = os.path.getsize(input_file)
+
+    # Use the content hash to identify the file in S3 to prevent unnecessary multiple uploads
+    upload_key = os.path.join(transcription_config.input_prefix, cached_hash_file_content(input_file).hex())
+
+    # Ensure that the audio file is uploaded to S3
+    try:
+        existing_object = aws_s3_client.head_object(Bucket=transcription_config.s3_bucket, Key=upload_key)
+
+        print("existing", file_size, existing_object['ContentLength'])
+
+        # In addition to the content hash in the key, this should protect against reading the wrong
+        # file (due to incomplete/partial uploads in the case of this check)
+        if file_size != existing_object['ContentLength']:
+            raise ExistingS3ObjectInvalid("Mismatched content length: {} (expected: {})".format(existing_object['ContentLength'], file_size))
+
+    # Object doesn't already exist
+    except (ExistingS3ObjectInvalid, BotoClientError) as e:
+        print("Uploading...")
+
+        # Upload the source file
+        aws_s3_client.upload_file(
+            Filename=input_file,
+            Bucket=transcription_config.s3_bucket,
+            Key=upload_key,
+            Callback=lambda bytes: upload_progress_callback(float(bytes) / file_size)
+        )
+    else:
+        print("Adopting existing S3 object")
+
+
+    # Ensure that there is a transcription job for this file and query
+    job_name = transcript_hash(input_file, transcription_options.digest())
+    transcript_file_name = transcript_name(input_file, transcription_options.digest())
+    try:
+        transcribe_job = aws_transcribe_client.get_transcription_job(
+            TranscriptionJobName=job_name
+        )
+
+        if transcribe_job['TranscriptionJob']['TranscriptionJobStatus'] == 'FAILED':
+            # Delete the old failed job so that we can re-use the name
+            aws_transcribe_client.delete_transcription_job(
+                TranscriptionJobName=job_name
+            )
+            raise "Previous job failed"
+
+    # We expect this unless a transcription job was already running or completed
+    except:
+        job_kwargs = {
+            "TranscriptionJobName": job_name,
+            "Media": {
+                "MediaFileUri":  "s3://" + os.path.join(transcription_config.s3_bucket, upload_key),
+            },
+            "OutputBucketName": transcription_config.s3_bucket,
+            "OutputKey": os.path.join(transcription_config.output_prefix, transcript_file_name),
+
+            # Disabled because this requires configuring IAM roles which is a bit of a PITA
+            # "JobExecutionSettings": {
+            #     "AllowDeferredExecution": True,
+            # }
+        }
+
+        # Fill out the Settings field if we need to
+        transcribe_settings = {}
+        if transcription_options.max_alternatives > 0:
+            transcribe_settings["ShowAlternatives"] = True
+            transcribe_settings["MaxAlternatives"] = transcription_options.max_alternatives
+        if len(transcribe_settings) > 0:
+            job_kwargs["Settings"] = transcribe_settings
+
+        # Either auto-detect language or specify it explicitly
+        if transcription_options.language_code is None:
+            job_kwargs["IdentifyLanguage"] = True
+        else:
+            job_kwargs["LanguageCode"] = transcription_options.language_code
+
+        print("Transcoding...")
+
+        transcribe_job = aws_transcribe_client.start_transcription_job(**job_kwargs)
+    else:
+        print("Adopting existing transcode job...")
+
+
+    transcribe_progress_callback(0.0)
+
+    print("Waiting...")
+
+    wait_counter = 0
+    while transcribe_job['TranscriptionJob']['TranscriptionJobStatus'] in ['QUEUED', 'IN_PROGRESS']:
+        wait_counter += 1
+
+        time.sleep(5)
+
+        transcribe_job = aws_transcribe_client.get_transcription_job(
+            TranscriptionJobName=job_name
+        )
+
+        # Make a fake progress counter which asymptotically approaches complete
+        progress = 0.75 - (1.0 / wait_counter)
+        if transcribe_job['TranscriptionJob']['TranscriptionJobStatus'] == 'QUEUED':
+            transcribe_progress_callback(progress)
+        elif transcribe_job['TranscriptionJob']['TranscriptionJobStatus'] == 'IN_PROGRESS':
+            transcribe_progress_callback(progress + 0.25)
+        elif transcribe_job['TranscriptionJob']['TranscriptionJobStatus'] == 'COMPLETED':
+            transcribe_progress_callback(1.0)
+
+    if transcribe_job['TranscriptionJob']['TranscriptionJobStatus'] != 'COMPLETED':
+        click.echo("Transcription job failed!\n{}".format(json.dumps(transcribe_job, sort_keys=True, indent=4)), err=True)
+        raise "Transcription error"
+
+    print("Downloading...")
+    local_transcript_path = os.path.join(os.path.dirname(input_file), transcript_file_name)
+
+    # Parse the given HTTP S3 URI into the correct format for boto to use
+    result_s3_bucket, *result_s3_key_components = urlparse(transcribe_job['TranscriptionJob']['Transcript']['TranscriptFileUri']).path.strip('/').split('/')
+    aws_s3_client.download_file(
+        Bucket=result_s3_bucket,
+        Key="/".join(result_s3_key_components),
+        Filename=local_transcript_path
+    )
+
+    return local_transcript_path
+
+CACHED_CONFIG = None
+def get_saved_config():
+    global CACHED_CONFIG
+
+    if CACHED_CONFIG is None:
+        try:
+            with open(SAVED_CONFIG_LOCATION, 'r') as f:
+                CACHED_CONFIG = json.loads(f.read())
+        except FileNotFoundError:
+            CACHED_CONFIG = {}
+
+    return CACHED_CONFIG
+
+def upsert_config(new_config):
+    config = get_saved_config()
+    config.update(new_config)
+
+    with open(SAVED_CONFIG_LOCATION, 'w+') as f:
+        f.write(json.dumps(config, sort_keys=True, indent=4))
+
+    click.echo("Config saved to ~/.speech-grep", err=True)
 
 @click.command(help="""
         Search audio transcripts with a grep-like interface. Currently only supports transcripts in the format produced by AWS Transcribe.
@@ -134,12 +344,19 @@ def ensure_transcripts(input_files):
 @click.option('--ignore-type', is_flag=True, help="By default inputs which don't look like audio files will be ignored. With this enabled, all files will be processed.")
 @click.option('--padding', '-p', type=float, default=0.05, help="Padding in seconds to apply around match timestamps")
 @click.option('--context', '-c', type=int, default=0, help="Number of words to show either side of the match")
-def grep(query, inputs, mode, ignore_type, padding, context):
+@click.option('--s3-bucket', type=str, default=lambda: os.environ.get('S3_BUCKET', get_saved_config().get('default_s3_bucket', '')), help="S3 bucket to use for AWS transcription")
+@click.option('--input-prefix', type=str, default="transcribe/inputs/", help="S3 prefix to use for uploaded audio files")
+@click.option('--output-prefix', type=str, default="transcribe/outputs/", help="S3 prefix to use for generated transcript files")
+def grep(query, inputs, mode, ignore_type, padding, context, s3_bucket, input_prefix, output_prefix):
     input_files = select_input_files(inputs, ignore_type)
 
-    ensure_transcripts(input_files)
+    # Allow callers to specify a working S3 bucket interactively
+    if s3_bucket == '':
+        s3_bucket = click.prompt('S3 Bucket not specified. Which S3 bucket would you like to use for transcoding?', type=str)
+        if click.confirm('Would you like to save this S3 bucket as the default for future use?'):
+            upsert_config({ "default_s3_bucket": s3_bucket })
 
-
+    transcript_path_by_input_path = ensure_transcripts(input_files, TranscriptionOptions(), TranscriptionConfig("aws_transcribe", s3_bucket, input_prefix, output_prefix))
 
 if __name__ == '__main__':
     grep()
